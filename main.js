a3324f368302ec5d1140066fd0e4e6549e26f69d
michael.lake <michael.lake@willowtreeapps.com>
2015-09-02 22:50:13 -0400

    detect mobile properly prevent zoom in mobile

--- main.js
+++ main.js
@@ -297,7 +297,7 @@ function setActiveLayers() {
     }
 }
 function focusIfDesktop(){
-    if ($(window).width() > 550) $('#search').focus();
+    if (!isMobile.any()) $('#search').focus();
 }
 
 function fetchTours() {
@@ -453,3 +453,23 @@ $(document).ready(function () {
 
 
 
+var isMobile = {
+    Android: function() {
+        return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function() {
+        return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: function() {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function() {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function() {
+        return navigator.userAgent.match(/IEMobile/i);
+    },
+    any: function() {
+        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+    }
+};
